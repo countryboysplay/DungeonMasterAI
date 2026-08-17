db4f74d0a98130a4cd95f77a49610272b28a3630
countryboysplay <31964073+countryboysplay@users.noreply.github.com>
2026-08-17 12:56:23 -0500

    r51 ui: fix Character portrait vignette namespace

--- windows/src/DungeonMasterAI.App/Views/CharactersView.xaml.cs
+++ windows/src/DungeonMasterAI.App/Views/CharactersView.xaml.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using DungeonMasterAI.App.Controls;
 
 namespace DungeonMasterAI.App.Views;
